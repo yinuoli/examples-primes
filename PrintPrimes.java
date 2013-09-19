956c683fda403de5bfd8254320cc84b8825a6d40
Aditya Mahajan <adityam@umich.edu>
2013-09-19 13:52:40 -0400

    The split
    The first attempt to split the functionality into two functions. It is a bad
    idea to create static variables and these will be removed in later commits.

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -7,56 +7,62 @@ public class PrintPrimes {
   static int P[] = new int[M + 1];
 
   public static void main(String[] args) {
-    boolean JPRIME;
-    int N;
-    int MULT[] = new int[ORDMAX + 1];
+      calculatePrimes();
+      printPrimes();
+  }
 
-    int J = 1;
-    int K = 1;
-    P[1] = 2;
-    int ORD = 2;
-    int SQUARE = 9;
+  public static void calculatePrimes() {
+      boolean JPRIME;
+      int N;
+      int MULT[] = new int[ORDMAX + 1];
 
-    while (K < M) {
-      do {
-        J = J + 2;
-        if (J == SQUARE) {
-          ORD = ORD + 1;
-          SQUARE = P[ORD] * P[ORD];
-          MULT[ORD - 1] = J;
-        }
-        N = 2;
-        JPRIME = true;
-        while (N < ORD && JPRIME) {
-          while (MULT[N] < J)
-            MULT[N] = MULT[N] + P[N] + P[N];
-          if (MULT[N] == J)
-            JPRIME = false;
-          N = N + 1;
-        }
-      } while (!JPRIME);
-      K = K + 1;
-      P[K] = J;
-    }
-    {
-      int PAGENUMBER = 1;
-      int PAGEOFFSET = 1;
-      while (PAGEOFFSET <= M) {
-        System.out.println("The First " + M +
-                             " Prime Numbers --- Page " + PAGENUMBER);
-        System.out.println("");
-        for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++){
-          for (int C = 0; C < CC;C++)
-            if (ROWOFFSET + C * RR <= M)
-              System.out.format("%10d", P[ROWOFFSET + C * RR]);
-          System.out.println("");
-        }
-        System.out.println("\f");
-        PAGENUMBER = PAGENUMBER + 1;
-        PAGEOFFSET = PAGEOFFSET + RR * CC;
+      int J = 1;
+      int K = 1;
+      P[1] = 2;
+      int ORD = 2;
+      int SQUARE = 9;
+
+      while (K < M) {
+        do {
+          J = J + 2;
+          if (J == SQUARE) {
+            ORD = ORD + 1;
+            SQUARE = P[ORD] * P[ORD];
+            MULT[ORD - 1] = J;
+          }
+          N = 2;
+          JPRIME = true;
+          while (N < ORD && JPRIME) {
+            while (MULT[N] < J)
+              MULT[N] = MULT[N] + P[N] + P[N];
+            if (MULT[N] == J)
+              JPRIME = false;
+            N = N + 1;
+          }
+        } while (!JPRIME);
+        K = K + 1;
+        P[K] = J;
       }
     }
-  }
+
+    public static void printPrimes() {
+        int PAGENUMBER = 1;
+        int PAGEOFFSET = 1;
+        while (PAGEOFFSET <= M) {
+          System.out.println("The First " + M +
+                               " Prime Numbers --- Page " + PAGENUMBER);
+          System.out.println("");
+          for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++){
+            for (int C = 0; C < CC;C++)
+              if (ROWOFFSET + C * RR <= M)
+                System.out.format("%10d", P[ROWOFFSET + C * RR]);
+            System.out.println("");
+          }
+          System.out.println("\f");
+          PAGENUMBER = PAGENUMBER + 1;
+          PAGEOFFSET = PAGEOFFSET + RR * CC;
+        }
+    }
 }
 
 					 
